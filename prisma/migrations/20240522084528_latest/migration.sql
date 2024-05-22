f7004ccd47459b2e2f4ee1aaf88c1e2e2b97b81e
ajay <ajayzyan100@gmail.com>
2024-05-22 14:17:14 +0530

    db(prisma): companion model  schema added

---
+++ prisma/migrations/20240522084528_latest/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE `Companion` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `userName` VARCHAR(191) NOT NULL,
+    `src` VARCHAR(191) NOT NULL,
+    `name` TEXT NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `instructions` TEXT NOT NULL,
+    `seed` TEXT NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `categoryId` VARCHAR(191) NOT NULL,
+
+    INDEX `Companion_categoryId_idx`(`categoryId`),
+    FULLTEXT INDEX `Companion_name_idx`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
